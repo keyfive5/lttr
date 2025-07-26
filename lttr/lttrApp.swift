d54a334cdbe1bf113bb7c928eccc42f85bf6ba36
Hasan <hzafar300@outlook.com>
2025-07-26 17:20:38 -0400

    Initial Commit

---
+++ lttr/lttrApp.swift
@@ -0,0 +1,17 @@
+//
+//  lttrApp.swift
+//  lttr
+//
+//  Created by Hasan Zafar on 2025-07-26.
+//
+
+import SwiftUI
+
+@main
+struct lttrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lttrTests/lttrTests.swift
@@ -0,0 +1,17 @@
+//
+//  lttrTests.swift
+//  lttrTests
+//
+//  Created by Hasan Zafar on 2025-07-26.
+//
+
+import Testing
+@testable import lttr
+
+struct lttrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
